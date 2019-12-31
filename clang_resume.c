6c91375fdd9b8b706853737e0e5c9fdf0b7f8ccb
caesar0m0d <caesar0m0d@gmail.com>
2019-12-31 23:25:14 +0900

    ./

--- clang_resume.c
+++ clang_resume.c
@@ -1,51 +1,100 @@
-/*****************************************************************/
-///////		R		E		S		U		M		E		///////	
-///////														///////
-///////		Myeong	-	Kyu			Kim						///////
-///////														///////
+///////*****************************************************************///////
+///////		R	E	S	U	M	E		///////	
+///////									///////
+///////		Myeong	-	Kyu			Kim		///////
+///////									///////
 ///////		82)10-6799-6764			hehe6764@hanmail.net	///////
-/*****************************************************************/
+///////*****************************************************************///////
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
 
-//certificate
-//experiment
-//education
-//paper
-//study
+#define indexxx	 	"./index.txt"
+#define edu_index	"./education_index.txt"
+
+#define lang		"./language.txt"
+#define edu		"./education.txt"
+#define paper		"./paper.txt"
+#define studies		"./studies.txt"
+#define namesize 30
+#define textlength 300
+
 int eng_skill=0;	//a.k.a. english skill
 
 
 typedef struct P{
 	
-	char * name;
-
+	char name[namesize];
+	char * resume_text;
+	int chk;
 	struct P * sib;//a.k.a. sibling
 	struct P * child;//a.k.a. sibling
 }indexx;
-
+indexx * root;
+indexx ** bfs_arr;
+int start,end;
+char * file_set[10];
 #include"education.h"
 #include"language.h"
 
 int main() {
 
 	int d, i, j, k, s, t, r, a;
-
-	indexx * education = (indexx *)malloc(sizeof(char) * 10);
-	indexx * language;
-	
-	alert(education);
+	char * file_name;
+	char buf_0[textlength];
+	int fd1 = open("./flush.txt",O_RDONLY);
 
 
-	//printf("%s\n", education->name);
-	printf("%s\n",education->name);
-	free(education);
+	file_set[0]=edu;
+	file_set[1]=lang;
+	file_set[2]=paper;
+	file_set[3]=studies;
+	bfs_arr = (indexx **)malloc(sizeof(indexx *) * 10);
+	root = (indexx *)malloc(sizeof(indexx) * 15);
+	root->sib=0;	
+	root->child =0;
+	file_name = indexxx;
+	node_set(root,file_name);
 
+	file_name = edu_index;
+	node_set(root->child,file_name);
+
+	bfs(root);
+		for(i=1;i<end;i++){
+			
+			printf("/");
+			for(j=0;j<namesize;j++)printf("*");
+			printf("/\n\n\n");
+			printf("\t\t%s\n\n\n",bfs_arr[i]->name);
+			printf("/");
+			for(j=0;j<namesize;j++)printf("*");
+			printf("/\n\n\n");
+			int fd;
+			if((fd = open(bfs_arr[i]->resume_text,O_RDONLY)) == -1)perror("No Open File");
+			read(fd,buf_0,textlength);
+			
+			printf("%s\n",buf_0);
+			printf("\n\n\n");
+			printf("automatically scrolled down after %d seconds\n\n\n\n",15);
+			
+			sleep(1);
+			for(k=1;k<15;k++){
+				sleep(1);
+				
+			}
+			
+			close(fd);
+		}
+	printf("press any key to quit\n");
 	getchar();
-
+	
 	return 0;
 }
 
--- education.h
+++ education.h
@@ -9,28 +9,37 @@
 
 #define buffsize 100
 
-int node_add(char * buf);
+static int edge_stat=0;
+	//0 is child
+	//1 is sibling
 
-int alert(indexx * education) {
 
+int node_add(char * buf,int edge_stat,indexx * parent);
+int copy(indexx * s_truct, char * buf);
+int bfs(indexx * root);
+
+int node_set(indexx * node, char * file_name) {
+
+	
 	int fd,i,j,sd;
 	char buf[buffsize];
-	if((fd = open("./education.txt",O_RDONLY)) == -1)perror("No Open File");
+	if((fd = open(file_name,O_RDONLY)) == -1)perror("No Open File");
 	
 	
 	for(j=0;fd != -1;j++){
 		i=0;
 		do{
 		sd=read(fd,buf+i,1);
-		}while(buf[i] > 65 && buf[i++]<123);
+		}while(buf[i] > 64 && buf[i++]<123);
 		buf[i]='\0';
+		
 		if(j!=0){
 			if(!sd)break;
-			//else node_add(buf);
+			else node_add(buf,edge_stat,node);
 
 		}
 		else if(j==0)
-			education->name = buf;//address coruption
+			copy(node,buf);//address coruption
 
 		
 		
@@ -42,11 +51,100 @@ int alert(indexx * education) {
 	close(fd);
 	return 0;
 }
-int node_add(char * buf){
-
-	//indexx * node = (indexx *)malloc(sizeof(char) * 10);
-	//node->name = *buf;
-
+int node_add(char * buf,int edge_stat, indexx * parent){
+	
+	int i;
+	indexx * temp = (indexx *)malloc(sizeof(indexx) * 10);
+	indexx * node = (indexx *)malloc(sizeof(indexx) * 10);
+	node->sib = 0;
+	node->child = 0;
+	node->chk = 0;
+	temp = parent->child;
+	copy(node,buf);
+	if(parent->child == 0){
+		parent->child = node;
+		
+	}
+		
+	else {
+		
+		for(;;){
+			if(temp->sib == 0){
+				temp->sib = node;
+				break;
+			}
+			else temp = temp->sib;
+			
+		}
+		
+	}
 
 return 0;
 }
+int copy(indexx * s_truct, char * buf){
+
+	int i;
+	char temp[10];
+	
+	for(i=0;buf[i]!='\0';i++)s_truct->name[i] = buf[i];
+
+
+
+return 0;
+
+}
+int bfs(indexx * root){
+
+	int i;
+	start=0,end=1;
+	
+	indexx * temp = (indexx *)malloc(sizeof(indexx) * 10);
+	bfs_arr[0] = root;
+
+
+	while(start != end){
+
+	temp = bfs_arr[start]->child;
+
+
+		if(temp != 0){
+			bfs_arr[end++]=temp;
+		
+		while(temp->sib != 0){
+			bfs_arr[end++]=temp->sib;
+			temp = temp->sib;
+		
+		}
+		}
+	start++;
+	}
+
+for(i=1;i<end;i++)bfs_arr[i]->resume_text=file_set[i-1];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
